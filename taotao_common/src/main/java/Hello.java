7b9ab87b6ee72cb9cf9944fb695a8891a2c95b5b
Youlingsong <1430665712@qq.com>
2018-04-06 22:14:57 +0800

    Update Hello.java

--- taotao_common/src/main/java/Hello.java
+++ taotao_common/src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("我，我爱你");
+        System.out.println("我喔喔");
         System.out.println("我你你，我爱你");
     }
 }
