ae5b3264cea289bec4f9f00ec43a8849d576e682
mackson <1430665712@qq.com>
2018-04-06 22:35:34 +0800

    删除测试

--- taotao_common/src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("我");
-        System.out.println("我哈");
-        System.out.println("我你你，我爱你");
-        System.out.println("我哈哈哈");
-        System.out.println("我你你");
-    }
-}
