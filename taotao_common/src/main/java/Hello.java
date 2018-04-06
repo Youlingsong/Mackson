5ff3a24f4a76a65475e7fc32bbba134e315ff59e
mackson <1430665712@qq.com>
2018-04-06 22:28:32 +0800

    提交到本地

--- taotao_common/src/main/java/Hello.java
+++ taotao_common/src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("我");
+        System.out.println("我哈哈哈");
         System.out.println("我你你，我爱你");
     }
 }
